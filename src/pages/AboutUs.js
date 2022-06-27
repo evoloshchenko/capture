cb3e7ed8c44f57006b5ffa122719e29ebe69a5e7
Jane Voloshchenko <j.voloshchenko@gmail.com>
2022-06-27 20:31:25 +0300

    Added FaqSection

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 //Import Components
 import AboutSection from "../component/AboutSection";
 import ServicesSection from "../component/ServicesSection";
+import FaqSection from "../component/FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />;
       <ServicesSection />;
+      <FaqSection />;
     </>
   );
 };
