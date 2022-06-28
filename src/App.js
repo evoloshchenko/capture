6b5acb591313ada3887990c8937b445e265399a6
Jane Voloshchenko <j.voloshchenko@gmail.com>
2022-06-28 20:00:39 +0300

    Added new side pages. And used react routers for linking them, and make it without refresh.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import GlobalStyle from "./component/GlobalStyle";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
 import Nav from "./component/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//Router
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" exact element={<AboutUs />} />
+        <Route path="/work" element={<OurWork />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
