ef26076f421f8cfa650718d488b7298f74b4a8bf
Jane Voloshchenko <j.voloshchenko@gmail.com>
2022-07-01 15:09:22 +0300

    Added a page transition animation

--- src/Animation.js
+++ src/Animation.js
@@ -6,23 +6,26 @@ export const pageAnimation = {
   show: {
     opacity: 1,
     y: 0,
-    transition: { duration: 1, when: "beforeChildren", staggerChildren: 0.25 },
+    transition: {
+      duration: 0.5,
+      when: "beforeChildren",
+      staggerChildren: 0.25,
+    },
   },
   exit: {
     opacity: 0,
-    y: 300,
     transition: { duration: 0.5 },
   },
 };
 
 export const titleAnimation = {
   hidden: { y: 600 },
-  show: { y: 0, transition: { duration: 0.75, ease: "linear" } },
+  show: { y: 0, transition: { duration: 0.75, ease: "easeInOut" } },
 };
 
 export const fade = {
   hidden: { opacity: 0 },
-  show: { opacity: 1, transition: { ease: "circOut", duration: 0.75 } },
+  show: { opacity: 1, transition: { ease: "linear", duration: 0.75 } },
 };
 
 export const photoAnimation = {
@@ -32,4 +35,26 @@ export const photoAnimation = {
     opacity: 1,
     transition: { ease: "circIn", duration: 0.75 },
   },
+};
+
+export const lineAnimation = {
+  hidden: { width: "0%" },
+  show: {
+    width: "100%",
+    transition: { duration: 1 },
+  },
+};
+
+export const slider = {
+  hidden: { x: "-130%", skew: "45deg" },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: { ease: "easeOut", duration: 1 },
+  },
+};
+
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: { opacity: 1, transition: { staggerChildren: 0.15, ease: "easeOut" } },
 };
