bd633de1bc5f68dad321a748a28a7c856005b94d
Jane Voloshchenko <j.voloshchenko@gmail.com>
2022-07-02 14:19:35 +0300

    Added a mobile responsivness to pages

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -45,14 +45,22 @@ const ContactStyle = styled(motion.div)`
   padding: 5rem 10rem;
   color: #353535;
   min-height: 90vh;
+  @media (max-width: 1300px) {
+    margin-top: 5rem;
+    padding: 2rem;
+    font-size: 1rem;
+  }
 `;
 
 const Title = styled.div`
   margin-bottom: 4rem;
   color: black;
-  h2{
+  h2 {
     font-size: 3rem;
     font-weight: bold;
+  }
+  @media (max-width: 1300px) {
+    margin-top: 5rem;
   }
 `;
 
